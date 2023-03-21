caae4385574715f2b29027a3c286e1616b3218ea
Marto-091 <78512135+Marto091@users.noreply.github.com>
2023-03-21 17:56:45 -0300

    Add files via upload

--- Portafolio Basico/js/index.js
+++ Portafolio Basico/js/index.js
@@ -1,14 +1,51 @@
-let open = false;
-
-const menu = () =>{ 
-    const barra = document.getElementById("menu")
-    if(open){
-        console.log('again')
-        barra.style.transform = 'rotate(180deg)'
-        open = false
+var project = document.getElementById("project")
+var sidemenu = document.getElementById("side-header")
+var aboutus = document.getElementById("about-us")
+var contact = document.getElementById("contact")
+var header = document.getElementById("header")
+var debuggeer = true
+const ProjectClick = () =>{
+    project.scrollIntoView({behavior:"smooth", block:"center", inline:"nearest"})
+    if(debuggeer){
+        sidemenu.style.display ='block'
+        debuggeer=false
     }else{
-        console.log('clicked')
-        open = true
-        barra.style.transform = 'rotate(90deg)'
+        sidemenu.style.display = 'none'
+        debuggeer=true
     }
+}
+
+const aboutusClick = () =>{
+    aboutus.scrollIntoView({behavior:"smooth", block:"center"})
+    if(debuggeer){
+        sidemenu.style.display ='block'
+        debuggeer=false
+    }else{
+        sidemenu.style.display = 'none'
+        debuggeer=true
+    }
+}
+const contactClick = () =>{
+    contact.scrollIntoView({behavior:"smooth", block:'end'})
+    if(debuggeer){
+        sidemenu.style.display ='block'
+        debuggeer=false
+    }else{
+        sidemenu.style.display = 'none'
+        debuggeer=true
+    }
+}
+
+const menuClick = (e) =>{
+    if(debuggeer){
+        sidemenu.style.display ='block'
+        debuggeer=false
+    }else{
+        sidemenu.style.display = 'none'
+        debuggeer=true
+    }
+}
+
+const arrowClick = () =>{
+    header.scrollIntoView({behavior:"smooth", block:"center"})
 }
